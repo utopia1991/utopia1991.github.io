1bf240dd95ec75b955d676215820b32d3fe719a6
Bevis Shen <1304354608@qq.com>
2020-03-08 19:50:22 +0800

    ticket 上传

---
+++ project/ticket/static/js/serviceConfig.js
@@ -0,0 +1,15 @@
+/*****
+ * Created with WebStorm.
+ * Date : 2018/9/8.
+ * Author : Ztr.
+ * Func : ..
+ ******/
+window.ServerConfig = {
+  // 测试库地址
+  base: 'http://112.125.89.96:8080',
+  testBase: 'http://localhost:5050',
+  imgBase: 'http://10.10.10.110:1301/onepay-web',
+  appId: '1DBHJRS5I01E0F34A8C00000B959FA87',
+  deviceId: '04daccefc14033ed3d18f157a9f6d1d8',
+  debug: true
+}
